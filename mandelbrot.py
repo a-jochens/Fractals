19c19079f55a01d2f7e7821be8727baa2fe47afc
Arne Jochens <A.Jochens@yahoo.com>
2023-03-25 00:37:00 +0100

    Completed basic functions to plot Mandelbrot set

--- mandelbrot.py
+++ mandelbrot.py
@@ -5,18 +5,37 @@
 # Implementation by Arne Jochens
 
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def transform(z, c):
+    """ Function to iterate for every point c within the frame. 
+        Works with single complex input numbers as well as with complex arrays.
+    """ 
+    return z**2 + c
+
+
+def plot_counts(iter_counts):
+    """ Plot NumPy array of integer values as heatmap. 
+    """
+    fig, ax = plt.subplots(figsize=[16, 10])
+    ax.set_aspect('equal')
+    cmap = sns.color_palette('mako', as_cmap=True)
+    sns.heatmap(iter_counts, ax=ax, cmap=cmap)
+    plt.show()
 
 
 def initialize_grid(frame, n_points_x, n_points_y):
-    """ Return a 2d NumPy array of points in the complex plane """
-
+    """ Return a 2d NumPy array of points in the complex plane. 
+    """
     bottom_left, top_right = frame
     x_start = bottom_left.real
     x_stop = top_right.real
     y_start = bottom_left.imag
     y_stop = top_right.imag
 
-    grid = np.empty(shape=[n_points_x, n_points_y], dtype='complex')
+    grid = np.zeros(shape=[n_points_y, n_points_x], dtype='complex')
 
     for j, y in enumerate(np.linspace(y_start, y_stop, n_points_y)):
         for i, x in enumerate(np.linspace(x_start, x_stop, n_points_x)):
@@ -25,18 +44,32 @@ def initialize_grid(frame, n_points_x, n_points_y):
     return grid
 
 
+def count_iterations(grid, n_iter):
+    """ Iterate the complex function n_iter times on the grid numbers.
+        Return a Boolean array indicating the numbers of iterations until divergence.
+    """
+    z = np.zeros(shape=grid.shape, dtype='complex')
+    diverged = np.zeros(shape=grid.shape, dtype='bool') 
+    iter_counts = np.zeros(shape=grid.shape, dtype='int') 
+
+    for i in range(n_iter):
+        z = transform(z * (~diverged).astype(int), grid)
+        diverged = diverged | (np.absolute(z) > 4)
+        iter_counts[~diverged] += 1
+
+    return iter_counts
+
 
 def picture_mandelbrot(frame=[complex(-2.25, -1.5), complex(0.75, 1.5)],
-                       n_points_x=3, n_points_y=3):
+                       n_points_x=1000, n_points_y=1000, n_iter=100):
     """ Picture the Mandelbrot set
         within the frame given as [bottom left corner, top right corner]
-        with a resolution of n_points_x times n_points_y pixels
+        with a resolution of n_points_x times n_points_y pixels.
     """
-
     grid = initialize_grid(frame=frame, n_points_x=n_points_x, n_points_y=n_points_y)
+    iter_counts = count_iterations(grid, n_iter)
+    plot_counts(iter_counts)
+    
 
-    print(grid)
-
-
-
-picture_mandelbrot()
+if __name__ == "__main__":
+    picture_mandelbrot()
